cc6d30402931040f56a437c8169052b500bef320
Сергей С. <87950098+Gresserg@users.noreply.github.com>
2025-06-06 01:32:39 +0300

    06.06.2025

--- tmpl/uikit-tmpl.php
+++ tmpl/uikit-tmpl.php
@@ -107,12 +107,12 @@ extract($displayData);
 
             if (!empty($order_details)) {
                 foreach ($order_details as $key => $value) {
-                    echo '<p><span class="order-info-label fw-bold">' . $key . '</span>: <span class="order-info-value">' . $value . '</span></p>';
+                    echo '<p><span class="order-info-label uk-text-bold">' . $key . '</span>: <span class="order-info-value">' . $value . '</span></p>';
                 }
                 if ($params->get('show_products', false) == true) { ?>
                     <div class="product-details-section mt-5 mb-5">
                         <?php foreach ($order->items as $item) {
-                            echo '<p class="mt-0 mb-0"><span class="order-info-label fw-bold">' . Text::_('JSHOP_NAME_PRODUCT') .
+                            echo '<p class="mt-0 mb-0"><span class="order-info-label uk-text-bold">' . Text::_('JSHOP_NAME_PRODUCT') .
                                 '</span>: <span class="order-info-value">' . $item->product_name . '</span></p>';
                         } ?>
                     </div>
@@ -142,7 +142,7 @@ extract($displayData);
                         <?php
                         echo '<p class="order-info-header fs-5">' . Text::_(string: 'PLG_WTJSHOPPINGADDORDERINFOTOCHECKOUTFINISH_SHOW_ORDER_ADDRESS') . '</p>';
                         foreach ($order_address as $key => $value) {
-                            echo '<p><span class="order-info-label fw-bold">' . $key . '</span>: <span class="order-info-value">' . $value . '</span></p>';
+                            echo '<p><span class="order-info-label uk-text-bold">' . $key . '</span>: <span class="order-info-value">' . $value . '</span></p>';
                         } ?>
                     </div>
                     <?php
